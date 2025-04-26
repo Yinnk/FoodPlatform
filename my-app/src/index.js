706a63536ec8460cf385b6197149a6f94dc1d46b
Yinnk <keyan.ying.e@gmail.com>
2025-04-25 17:29:08 -0700

    set up firebase, change repo structure

---
+++ my-app/src/index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVyFJyNKYzKLjjoNa6sb8IkZRjWcxKS3E",
+  authDomain: "info-442-1ecfa.firebaseapp.com",
+  databaseURL: "https://info-442-1ecfa-default-rtdb.firebaseio.com",
+  projectId: "info-442-1ecfa",
+  storageBucket: "info-442-1ecfa.firebasestorage.app",
+  messagingSenderId: "155372170129",
+  appId: "1:155372170129:web:354f8adbfb5b8d0e0c8830",
+  measurementId: "G-G4ZPFVCC24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
