773d26da1a553516ff1fe6e3f10d6d24e333ee2e
Michelle Ngo <mngo4@uw.edu>
2025-04-30 10:33:13 -0700

    added data

--- my-app/src/data/Restaurant.js
+++ my-app/src/data/Restaurant.js
@@ -1,3 +1,41 @@
-export function CardGrid({props}){
-    
-}
+const restaurants = [
+    {
+      name: "Spice Bridge",
+      type: "Food Hall",
+      price: 15,
+      image: null,
+      location: "14200 Tukwila International Blvd, Tukwila, WA",
+      pickUpTime: "4:00 PM - 6:00 PM",
+      foodSurplus: "High", // UI maps this to green star/icon
+      vegan: "Vegan",
+      glutenFree: "Gluten-Free",
+      halal: "Halal",
+      nutFree: "Nut-Free"
+    },
+    {
+      name: "Bai Tong Restaurant",
+      type: "Thai",
+      price: 18,
+      image: null,
+      location: "16876 Southcenter Parkway, Tukwila, WA",
+      pickUpTime: "5:00 PM - 7:00 PM",
+      foodSurplus: "Medium", // UI maps this to orange star/icon
+      vegan: "Not Vegan",
+      glutenFree: "Gluten-Free",
+      halal: "Contains Non-Halal Items",
+      nutFree: "Contains Nuts"
+    },
+    {
+      name: "Old Saigon",
+      type: "Vietnamese",
+      price: 12,
+      image: null,
+      location: "971 Industry Drive, Tukwila, WA",
+      pickUpTime: "3:30 PM - 5:30 PM",
+      foodSurplus: "Low", // UI maps this to red star/icon
+      vegan: "Vegan",
+      glutenFree: "Gluten-Free",
+      halal: "Contains Non-Halal Items",
+      nutFree: "Contains Nuts"
+    }
+  ];
