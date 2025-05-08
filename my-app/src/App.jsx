e766336d3958adb96347f9abd69c39df966725f2
Yinnk <keyan.ying.e@gmail.com>
2025-05-07 22:49:00 -0700

    change food surplus icon to the correct color

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,8 +4,6 @@ import { SignInPage } from './pages/SignInPage.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage.jsx';
-import { RestaurantCard } from './components/RestaurantCard.jsx';
-import { CardGrid } from './components/CardGrid.jsx';
 import  {RestaurantPage} from './pages/RestaurantPage.jsx';
 
 
@@ -17,7 +15,6 @@ function App() {
        <Routes>
        <Route path="/" element={<HomePage />} />
        <Route path="/signin" element={<SignInPage />} /> 
-       <Route path="/card" element={<CardGrid />} />  
        <Route path="/restaurantpage" element={<RestaurantPage />} />   
         <Route path="/orderpage" element={<h1>Order Page</h1>} />{/* place holder for order page */}
        </Routes>
