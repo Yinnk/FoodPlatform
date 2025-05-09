a776c3c5469e9853c30dcf9299a0d6e9044e4489
Yinnk <keyan.ying.e@gmail.com>
2025-05-08 17:23:10 -0700

    adding explore button logic

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import './restaurant-hero.css'
+import { useNavigate } from 'react-router-dom'; 
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 const HeroSection = () => {
+  const navigate = useNavigate();
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+ useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsSignedIn(true);
+      } else {
+        setIsSignedIn(false);
+      }
+    });
+
+    return () => unsubscribe();
+  });
+
+  const handleExploreClick = () => {
+    if (isSignedIn) {
+      navigate('/restaurantpage');
+    } else {
+      navigate('/signin');
+    }
+  }
+
+
+
   return (
     <section className="hero flex flex-col md:flex-row items-center justify-between px-6 py-12 bg-green-50">
       <div className="hero-content md:w-1/2 mb-8 md:mb-0">
@@ -11,7 +40,7 @@ const HeroSection = () => {
         <p className="text-gray-700 mb-6">
           Discover and reserve surplus meals from local Seattle restaurants—fresh, affordable, and eco-friendly.
         </p>
-        <button className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition">
+        <button className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition" onClick={handleExploreClick}>
           Explore Now
         </button>
       </div>
