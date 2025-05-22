ecb869d722db91c791aa9f56da398aa8fd873075
harshita singh <165843455+harshitaSingh121@users.noreply.github.com>
2025-05-22 14:21:45 -0700

    Update RestaurantFilter.jsx
    fix cuisine filter

--- my-app/src/components/RestaurantFilter.jsx
+++ my-app/src/components/RestaurantFilter.jsx
@@ -1,48 +1,6 @@
 import React, { useState } from 'react';
+import { restaurants } from '../data/Restaurant.js';
 
-
-// Restaurant data
-const restaurants = [
-  {
-    name: "Spice Bridge",
-    type: "Food Hall",
-    price: 15,
-    image: null,
-    location: "14200 Tukwila International Blvd, Tukwila, WA",
-    pickUpTime: "4:00 PM - 6:00 PM",
-    foodSurplus: "High", 
-    vegan: "Vegan",
-    glutenFree: "Gluten-Free",
-    halal: "Halal",
-    nutFree: "Nut-Free"
-  },
-  {
-    name: "Bai Tong Restaurant",
-    type: "Thai",
-    price: 18,
-    image: null,
-    location: "16876 Southcenter Parkway, Tukwila, WA",
-    pickUpTime: "5:00 PM - 7:00 PM",
-    foodSurplus: "Medium", 
-    vegan: "Not Vegan",
-    glutenFree: "Gluten-Free",
-    halal: "Contains Non-Halal Items",
-    nutFree: "Contains Nuts"
-  },
-  {
-    name: "Old Saigon",
-    type: "Vietnamese",
-    price: 12,
-    image: null,
-    location: "971 Industry Drive, Tukwila, WA",
-    pickUpTime: "3:30 PM - 5:30 PM",
-    foodSurplus: "Low", 
-    vegan: "Vegan",
-    glutenFree: "Gluten-Free",
-    halal: "Contains Non-Halal Items",
-    nutFree: "Contains Nuts"
-  }
-];
 
 const RestaurantFilter = () => {
   // State to store the selected filter options
